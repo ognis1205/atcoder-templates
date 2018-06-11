924401708680252c14f4691c308ca53d509566dd
ognis1205 <shingo.okawa.g.h.c@gmail.com>
2018-06-12 08:49:01 +0900

    add python template.

---
+++ Python/main.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+try:
+   input = raw_input
+except NameError:
+   pass
+
+import os
+import sys
+from copy import deepcopy
+from math import sqrt
+from itertools import permutations
+from itertools import combinations
+from traceback import format_exc
+
+
+def get_line():
+    return input()
+
+def get_line_as(parse, in_array=False):
+    return map(parse, get_line().split()) if in_array else map(parse, get_line().split())[0]
+
+def array(n, value=0):
+    return [deepcopy(value) for i in range(n)]
+
+def main():
+    return
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except:
+        print(format_exc(), file=sys.stderr)
