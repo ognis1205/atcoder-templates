c238471066a3bbbf496f19d85d161ddd964cbca5
ognis1205 <shingo.okawa.g.h.c@gmail.com>
2018-06-17 08:36:11 +0900

    add template for HackerRank.

---
+++ Java/Solution.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    private static Scanner scanner;
+    private static Solution solver;
+
+    public static void main(String[] args) {
+        solver = new Solution();
+        scanner = new Scanner(System.in);
+
+        try {
+            solver.solve();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void solve() {
+        System.out.println();
+    }
+}
